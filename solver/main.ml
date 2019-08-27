afb4290e2912277eb934160579afc2051ec7b398
cepalle <epalle.cyrille@gmail.com>
2019-08-27 10:28:02 +0200

    read stdin

--- solver/main.ml
+++ solver/main.ml
@@ -1,1 +1,19 @@
-print_string "Hello world!\n"
+let rec hilo n =
+  let () = print_string "type a number: " in
+  let i = read_int () in
+  if i = n then
+    let () = print_string "BRAVO" in
+    let () = print_newline () in
+    print_newline ()
+  else
+    let () =
+      if i < n then
+        let () = print_string "Higher" in
+        print_newline ()
+      else
+        let () = print_string "Lower" in
+        print_newline ()
+    in
+      hilo n
+      
+let _ = hilo 64

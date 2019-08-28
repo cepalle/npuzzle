92dea9b13267bea85ba827a0ec08a04cc8ccfa9f
cepalle <epalle.cyrille@gmail.com>
2019-08-28 12:05:58 +0200

    A* start

--- solver/main.ml
+++ solver/main.ml
@@ -10,28 +10,44 @@ type np_node = {
   score: int;
 }
 
+let e_moves = Up::Down::Right::Left::[]
+
 (* TODO *)
 let grd_move (grd: int list list) (m: e_move): int list list = grd
 
-let np_node_move {cost=cost; hys=hys; grd=grd} (m: e_move) (scoring: int list list -> int): np_node =
+let np_node_move {cost=cost; hys=hys; grd=grd} (m: e_move) (scoring_node: np_node -> int): np_node =
   let nwgrd = grd_move grd m in
   let nwcost = cost + 1 in {
     cost = nwcost;
     hys = m :: hys;
     grd = nwgrd;
-    score = scoring nwgrd + nwcost;
+    score = scoring_node {grd=nwgrd; cost=nwcost; hys=[]; score=0};
   }
 
 (* TODO *)
 let is_solvable (grd: int list list): bool = true
 
 (* TODO *)
+let is_resolve ({grd=grd} : np_node): bool = true
+
+(* TODO *)
 let scoring_grd_manhattan (grd: int list list): int = 0
 
-let scoring_node ({score=score; cost=cost}: np_node) (w: int) (greedy: bool): int =
-  w * score + if greedy then 0 else cost
+(* TODO *)
+let add_in_prio_queu (to_add: np_node list) (opened: np_node list): np_node list = []
 
-let a_start_solver (scoring_grd: int list list -> int) (w: int) (greedy: bool): np_node =
+let scoring_node (scoring_grd: int list list -> int) (w: int) (greedy: bool) ({grd=grd; cost=cost}: np_node): int =
+  w * (scoring_grd grd) + if greedy then 0 else cost
+
+let a_start_solver (scoring_node: np_node -> int) (start: np_node): np_node =
+  let closed = Hashtbl.create (1024 * 1024) in
+  let opened = ref ([start]: np_node list) in
+  while List.exists (fun e -> true) !opened && not (is_resolve (hd !opened)) do
+    let frst = hd !opened in
+    let v = List.filter_map (fun m -> np_node_move first m) e_moves  in
+    v
+  done;
+  start
 
 ;;
 

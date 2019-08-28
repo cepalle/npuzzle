35c8b7a4fa121c07f01408fad50b922c03de8d0b
cepalle <epalle.cyrille@gmail.com>
2019-08-28 15:14:15 +0200

    A* up

--- solver/main.ml
+++ solver/main.ml
@@ -3,6 +3,8 @@ open List
 
 type e_move = Up | Down | Right | Left
 
+let e_moves = Up::Down::Right::Left::[]
+
 type np_node = {
   cost: int;
   hys: e_move list;
@@ -10,25 +12,29 @@ type np_node = {
   score: int;
 }
 
-let e_moves = Up::Down::Right::Left::[]
+let np_node_get_grd ({grd=grd}: np_node) : int list list = grd
 
 (* TODO *)
-let grd_move (grd: int list list) (m: e_move): int list list = grd
+let grd_move (grd: int list list) (m: e_move): int list list option = Some grd
 
-let np_node_move {cost=cost; hys=hys; grd=grd} (m: e_move) (scoring_node: np_node -> int): np_node =
+let np_node_move {cost=cost; hys=hys; grd=grd} (m: e_move) (scoring_node: np_node -> int): np_node option =
   let nwgrd = grd_move grd m in
-  let nwcost = cost + 1 in {
-    cost = nwcost;
-    hys = m :: hys;
-    grd = nwgrd;
-    score = scoring_node {grd=nwgrd; cost=nwcost; hys=[]; score=0};
-  }
+  if nwgrd == None then
+    None
+  else
+    let nwcost = cost + 1 in
+    Some {
+      cost = nwcost;
+      hys = m :: hys;
+      grd = (Option.get nwgrd);
+      score = scoring_node {grd=(Option.get nwgrd); cost=nwcost; hys=[]; score=0};
+    }
 
 (* TODO *)
 let is_solvable (grd: int list list): bool = true
 
 (* TODO *)
-let is_resolve ({grd=grd} : np_node): bool = true
+let is_resolve (grd: int list list): bool = true
 
 (* TODO *)
 let scoring_grd_manhattan (grd: int list list): int = 0
@@ -39,13 +45,17 @@ let add_in_prio_queu (to_add: np_node list) (opened: np_node list): np_node list
 let scoring_node (scoring_grd: int list list -> int) (w: int) (greedy: bool) ({grd=grd; cost=cost}: np_node): int =
   w * (scoring_grd grd) + if greedy then 0 else cost
 
+(* TODO max len + nb elements tot *)
 let a_start_solver (scoring_node: np_node -> int) (start: np_node): np_node =
   let closed = Hashtbl.create (1024 * 1024) in
   let opened = ref ([start]: np_node list) in
-  while List.exists (fun e -> true) !opened && not (is_resolve (hd !opened)) do
-    let frst = hd !opened in
-    let v = List.filter_map (fun m -> np_node_move first m) e_moves  in
-    v
+  while List.exists (fun e -> true) !opened && not (is_resolve (np_node_get_grd (hd !opened))) do
+    let (frst: np_node) = hd !opened in
+    let (neighbours: np_node list) = List.filter_map (fun m -> np_node_move frst m scoring_node) e_moves in
+    let neighbours_not_in_closed = List.filter (fun e -> (Hashtbl.find_opt closed (np_node_get_grd frst)) == None) neighbours in
+    opened := add_in_prio_queu neighbours_not_in_closed (tl !opened);
+    Hashtbl.add closed (np_node_get_grd frst) true;
+    neighbours
   done;
   start
 
